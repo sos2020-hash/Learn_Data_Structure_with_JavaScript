00117f7bd52a2eb338a13cd6a58317833722a02b
sos2020-hash <qweiop525@gmail.com>
2021-10-07 15:58:14 +0800

    add DoublyLinkedList update Node

--- linkedList/Node.js
+++ linkedList/Node.js
@@ -2,17 +2,31 @@ class Node {
   constructor(data) {
     this.data = data;
     this.next = null;
+    this.previous = null;
   }
 
   setNextNode(node) {
-    if (!(node instanceof Node)) {
+    if (node instanceof Node || node === null) {
+      this.next = node;
+    } else {
       throw new Error("Next node must be a member of the Node class");
     }
-    this.next = node;
+  }
+
+  setPreviousNode(node) {
+    if (node instanceof Node || node === null) {
+      this.previous = node;
+    } else {
+      throw new Error("Previous node must be a member of the Node class");
+    }
   }
 
   getNextNode() {
     return this.next;
+  }
+
+  getPreviousNode() {
+    return this.previous;
   }
 }
 
